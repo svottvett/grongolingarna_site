53c2120a6416866708b3c406f16263d149dee5a1
Martin <tjenamartin@gmail.com>
2018-04-12 20:05:35 +0200

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: {
+        App: "./app/assets/scripts/App.js",
+        Vendor:"./app/assets/scripts/Vendor.js"
+    },
+    output: {
+        path: __dirname+"/app/temp/scripts",
+        filename: "[name].js"    
+    },
+    
+    module : {
+        rules: [
+            {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                },
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
+    }
+}
